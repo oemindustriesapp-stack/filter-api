21ee8176ed0b8280ec75f84a00cc31bb8941a562
oemindustriesapp-stack <oemindustriesapp@gmail.com>
2026-04-15 11:17:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use((req, res, next) => {
   res.setHeader(
     "Access-Control-Allow-Origin",
-    "https://oem-industries.zohoecommerce.com"
+    "https://www.oemcatalyticconverters.com"
   );
   res.setHeader(
     "Access-Control-Allow-Methods",
